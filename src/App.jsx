bc477707481e13465525a2403400ef2fe30d1c02
Anvesh Dunna <anvesh.dunna@gmail.com>
2023-05-15 16:23:26 +0530

    Pushed first icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import IconList from "./components/IconList";
 
 function App() {
   return (
-    <>
-      <div className="text-gray-500 text-title3 font-semibold">Icons</div>
-      <div>Search (coming soon)</div>
-    </>
+    <div className="flex flex-col items-start">
+      <div className="text-title3 font-semibold text-gray-500">Icons</div>
+      {/* <div>Search (coming soon)</div> */}
+      <IconList />
+    </div>
   );
 }
 
--- src/components/IconList.jsx
+++ src/components/IconList.jsx
@@ -1,5 +1,26 @@
-const IconList = () => {
-  return <div>IconList</div>;
+import Icon, { IconMap } from "../assets/Icon";
+
+let iconArray = Object.entries(IconMap);
+
+const IconList = (props) => {
+  return (
+    <div className="mt-8 flex flex-col gap-4">
+      {iconArray.map((a, i) => {
+        let [x, y] = a;
+        return (
+          <div
+            key={i}
+            className="flex items-center gap-2 text-body2 text-gray-700"
+          >
+            <div>
+              <Icon name={x} color="currentColor" />
+            </div>
+            <span>{y.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default IconList;
